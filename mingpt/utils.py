6c5c5248aa3e9306ef9900c9c55f17f25d297ba0
ivannz <ivannz@yandex.ru>
2020-12-05 20:38:48 +0300

    more economical generator

--- mingpt/utils.py
+++ mingpt/utils.py
@@ -1,7 +1,8 @@
 import random
-import numpy as np
+
 import torch
-import torch.nn as nn
+import numpy as np
+
 from torch.nn import functional as F
 
 
@@ -12,15 +13,12 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
 
 
-def top_k_logits(logits, k):
+def top_k_logits_(logits, k):
     v, ix = torch.topk(logits, k)
-    out = logits.clone()
-    out[out < v[:, [-1]]] = -float('Inf')
-    return out
+    return logits.masked_fill_(logits.lt(v[:, [-1]]), float('-inf'))
 
 
-@torch.no_grad()
-def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
+def generate(model, x, steps, temperature=1.0, sample=False, top_k=None):
     """
     take a conditioning sequence of indices in x (of shape (b,t)) and predict the next token in
     the sequence, feeding the predictions back into the model each time. Clearly the sampling
@@ -28,15 +26,24 @@ def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
     of block_size, unlike an RNN that has an infinite context window.
     """
     block_size = model.get_block_size()
+
+    # essentially a crude circular buffer for characters
+    i0, i1 = 0, min(len(x), block_size)
+    state = torch.empty(2 * block_size, dtype=x.dtype, device=x.device)
+    state[i0:i1].copy_(x[-block_size:])  # crop context if needed
+
     model.eval()
     for k in range(steps):
-        x_cond = x if x.size(1) <= block_size else x[:, -block_size:]  # crop context if needed
-        logits, _ = model(x_cond)
+        with torch.no_grad():
+            logits, _ = model(state[i0:i1].unsqueeze(0))
+
         # pluck the logits at the final step and scale by temperature
         logits = logits[:, -1, :] / temperature
+
         # optionally crop probabilities to only the top k options
         if top_k is not None:
-            logits = top_k_logits(logits, top_k)
+            logits = top_k_logits_(logits, top_k)
+
         # apply softmax to convert to probabilities
         probs = F.softmax(logits, dim=-1)
         # sample from the distribution or take the most likely
@@ -44,7 +51,24 @@ def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
             ix = torch.multinomial(probs, num_samples=1)
         else:
             _, ix = torch.topk(probs, k=1, dim=-1)
-        # append to the sequence and continue
-        x = torch.cat((x, ix), dim=1)
 
-    return x
+        # append to the sequence and continue
+        if i1 >= len(state):
+            state[:i1 - i0].copy_(state[i0:i1])
+            i0, i1 = 0, i1 - i0
+        ix = state[i1] = int(ix)
+
+        yield ix
+
+        # shift the context in the buffer
+        i1 += 1
+        if i1 - i0 >= block_size:  # can only be == in fact!
+            i0 += 1
+
+
+@torch.no_grad()
+def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
+    result = list(generate(model, x, steps, temperature=temperature,
+                           sample=sample, top_k=top_k))
+
+    return torch.cat([x, torch.tensor(result).to(x)])
